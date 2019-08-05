866419be3a93fd15217ee1f7f87d93eb1ac341d5
Štěpán Beneš <benes.stepan@gmail.com>
2019-08-05 19:23:23 +0200

    Add CpuTemperatureService and CpuTemperature razor page.

---
+++ Data/CpuTemperatureService.cs
@@ -0,0 +1,18 @@
+﻿using Iot.Device.CpuTemperature;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cloudberry.Data
+{
+	public class CpuTemperatureService
+	{
+		public double GetTemperatureInCelsius()
+		{
+			var cpuTempProvider = new CpuTemperature();
+			var cpuTemp = cpuTempProvider.Temperature;
+			return cpuTemp.Celsius;
+		}
+	}
+}
--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace Cloudberry
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
 			services.AddSingleton<WeatherForecastService>();
+			services.AddSingleton<CpuTemperatureService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
