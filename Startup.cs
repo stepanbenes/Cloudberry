68659754b9e003e38e5e003545e0aa7ca585b813
Štěpán Beneš <benes.stepan@gmail.com>
2019-08-08 11:27:22 +0200

    Use forwarded headers to allow work with proxy servers.

--- Startup.cs
+++ Startup.cs
@@ -35,6 +35,11 @@ namespace Cloudberry
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			app.UseForwardedHeaders(new ForwardedHeadersOptions
+			{
+				ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All
+			});
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
