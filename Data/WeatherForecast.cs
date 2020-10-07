0c7f6755dc7884b4ec24455121b89d8b54cdf4fb
Štěpán Beneš <benes.stepan@gmail.com>
2020-10-07 22:17:36 +0200

    Upgrade to .net 5.0, C# 9.0 preview.

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -2,14 +2,22 @@ using System;
 
 namespace Cloudberry.Data
 {
-	public class WeatherForecast
+	public record WeatherForecast
 	{
-		public DateTime Date { get; set; }
-
-		public int TemperatureC { get; set; }
-
+		public DateTime Date { get; init; }
+		public int TemperatureC { get; init; }
+		public string Summary { get; init; }
 		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-		public string Summary { get; set; }
 	}
+
+	//public class WeatherForecast
+	//{
+	//	public DateTime Date { get; set; }
+
+	//	public int TemperatureC { get; set; }
+
+	//	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+	//	public string Summary { get; set; }
+	//}
 }
