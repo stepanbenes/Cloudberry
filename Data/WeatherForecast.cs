91ed795b591c2d3ef45ff3060f7df97553ce0b9b
Štěpán Beneš <benes.stepan@gmail.com>
2020-10-09 22:50:46 +0200

    Revert to .netcoreapp3.1 target framework.

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -2,22 +2,22 @@ using System;
 
 namespace Cloudberry.Data
 {
-	public record WeatherForecast
+	// public record WeatherForecast
+	// {
+	// 	public DateTime Date { get; init; }
+	// 	public int TemperatureC { get; init; }
+	// 	public string Summary { get; init; }
+	// 	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+	// }
+
+	public class WeatherForecast
 	{
-		public DateTime Date { get; init; }
-		public int TemperatureC { get; init; }
-		public string Summary { get; init; }
+		public DateTime Date { get; set; }
+
+		public int TemperatureC { get; set; }
+
 		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+		public string Summary { get; set; }
 	}
-
-	//public class WeatherForecast
-	//{
-	//	public DateTime Date { get; set; }
-
-	//	public int TemperatureC { get; set; }
-
-	//	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-	//	public string Summary { get; set; }
-	//}
 }
