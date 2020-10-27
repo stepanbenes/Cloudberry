d4d2009277d91deae18575c45f21cf795c549968
Štěpán Beneš <benes.stepan@gmail.com>
2020-10-28 00:41:16 +0100

    Use developer exception page

--- Startup.cs
+++ Startup.cs
@@ -43,16 +43,11 @@ namespace Cloudberry
 				ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All
 			});
 
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+			app.UseDeveloperExceptionPage();
+
+			//app.UseExceptionHandler("/Home/Error");
+			//// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			//app.UseHsts();
 
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
